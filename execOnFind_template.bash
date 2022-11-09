5b2f1e84ed9b70a5ba5b41606afafb7af68dc482
Austin McCullough <46095417+mcculla@users.noreply.github.com>
2022-11-09 09:41:54 -0600

    Add files via upload

---
+++ execOnFind_template.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# author: Austin McCullough
+# last modified: 10-31-2022 Austin McCullough
+# description:
+# inputs:
+# outputs:
+
+find . -type f -name "*.4dfp.ifh" | while read -r file; do
+	
+done
+
+
+
+
